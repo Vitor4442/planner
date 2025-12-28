5a056c804532b3c752134f52bc2a8d644d72bf3b
José Vitor <jose.vitorw018@gmail.com>
2025-12-28 18:06:31 -0300

    Criando o endpoint de confirmação de participante

---
+++ src/main/java/com/airplane/planner/participant/ParticapantController.java
@@ -0,0 +1,35 @@
+package com.airplane.planner.participant;
+
+import com.airplane.planner.trip.Trip;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/participants")
+public class ParticapantController {
+
+    @Autowired
+    private ParticapantRepository repository;
+
+    @PostMapping("/{id}/confirm")
+    public ResponseEntity<Participant> confirmParticipant(@PathVariable UUID id, @RequestBody ParticipantRequestPayload payload){
+        Optional<Participant> participant = repository.findById(id);
+
+        if(participant.isPresent()){
+            Participant rawParticipant = participant.get();
+            rawParticipant.setConfirmed(true);
+            rawParticipant.setName(payload.name());
+
+            this.repository.save(rawParticipant);
+
+            return ResponseEntity.ok(rawParticipant);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+}

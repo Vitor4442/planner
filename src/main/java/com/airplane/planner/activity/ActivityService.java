79b4029706f96836dadfc7e9d17b88fd3df97dea
José Vitor <jose.vitorw018@gmail.com>
2025-12-28 23:58:47 -0300

    Validação de atividades

--- src/main/java/com/airplane/planner/activity/ActivityService.java
+++ src/main/java/com/airplane/planner/activity/ActivityService.java
@@ -4,6 +4,7 @@ import com.airplane.planner.trip.Trip;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,7 +16,15 @@ public class ActivityService {
 
     public ActivityResponse registerActivity(ActivityRequestPayload payload, Trip trip){
         Activity newActivity = new Activity(payload.title(), payload.occurs_at(), trip);
-        
+
+        LocalDateTime occursAt = newActivity.getOccursAt();
+
+        if(occursAt.isBefore(trip.getStartAt()) || occursAt.isAfter(trip.getEndsAt())){
+            throw new IllegalArgumentException(
+                    "A atividade deve ocorrer dentro do período da viagem"
+            );
+        }
+
         this.repository.save(newActivity);
         
         return new ActivityResponse(newActivity.getId());
@@ -24,4 +33,6 @@ public class ActivityService {
     public List<ActivityData> getAllActivitiesFromId(UUID tripId){
         return this.repository.findByTripId(tripId).stream().map(activity -> new ActivityData(activity.getId(), activity.getTitle(), activity.getOccursAt())).toList();
     }
+
+
 }
